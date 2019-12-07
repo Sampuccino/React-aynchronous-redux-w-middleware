14dce52aa222d1a86fe210fc5c86547b6fcb4ced
Sampuccino <Sampuccino@gmail.com>
2019-12-06 20:25:23 -0800

    A little bit more progression with Redux and the sanmple external api.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,9 +8,33 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   }
 
+  renderList(){
+    return this.props.posts.map( post => {
+      return (
+        <div className='item' key={post.id}>
+          <i className='large middle aligned icon user'/>
+          <div className="left aligned content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  }
+
   render() {
-    return <div>Post List</div>
+    return <div className='ui relaxed divided list'>
+      {this.renderList()}
+    </div>
   }
 }
+
+
+const mapStateToProps = (state) => {
+  return { posts: state.posts }
+};
+
 // Currently NULL as no state to
-export default connect(null, {fetchPosts} )(PostList);
+export default connect(mapStateToProps, {fetchPosts} )(PostList);
