67ad5a88f43599f6a4db54a3b106ca351aacb7f9
Sampuccino <Sampuccino@gmail.com>
2019-12-07 21:24:08 -0800

    End of chapter. A good little overview of Redux store design and use of Reducers, Actions & State.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchPosts} from "../actions";
+import {fetchPostsAndUsers} from "../actions";
+
+import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
 
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList(){
@@ -18,6 +20,9 @@ class PostList extends React.Component {
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+
+            <UserHeader userId={post.userId}/>
+
           </div>
         </div>
       );
@@ -37,4 +42,4 @@ const mapStateToProps = (state) => {
 };
 
 // Currently NULL as no state to
-export default connect(mapStateToProps, {fetchPosts} )(PostList);
+export default connect(mapStateToProps, {fetchPostsAndUsers} )(PostList);
